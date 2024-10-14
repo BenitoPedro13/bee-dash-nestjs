82c87934658403d9dd7ffa1b1048a44fd2310060
Benito Pedro <benitopedro21@outlook.com>
2024-10-14 09:33:37 -0300

    🐛 FIX: module providers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CreatorsService } from 'src/creators/creators.service';
+import { CampaignsService } from 'src/campaigns/campaigns.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, CreatorsService, CampaignsService],
 })
 export class UsersModule {}
